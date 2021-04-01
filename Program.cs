2117e69c5e676f76c64d436729ca35fc54839dac
Björn Ekelund <bjorn@ekelund.nu>
2021-04-01 09:57:43 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace gridmatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string grid = "JO65MR";
+            int height = 5;
+            int width = 5;
+
+            string[,] array = GridArray(grid, height, width);
+
+            for (int vert = 0; vert < height; vert++)
+            {
+                for (int hor = 0; hor < width; hor++)
+                {
+                    Console.Write(array[vert, hor] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static private string [,] GridArray(string square, int height, int width)
+        {
+            string[,] result = new string[height,width];
+
+            for (int vert = 0; vert < height; vert++)
+            {
+                for (int hor = 0; hor < width; hor++)
+                {
+                    result[vert, hor] = vert.ToString("00") + "-" + hor.ToString("00");
+                }
+            }
+
+            return result;
         }
     }
 }
