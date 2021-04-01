3893137d4dcd03b0f2770ecb75b1bc09619a0740
Björn Ekelund <bjorn@ekelund.nu>
2021-04-01 13:20:24 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -7,18 +7,27 @@ namespace gridmatrix
         static void Main(string[] args)
         {
             string grid = "RR85MR";
-            int height = 15;
-            int width = 15;
+            int height = 9;
+            int width = 9;
 
-            string[,] array = GridMatrix(grid, height, width);
-
+            string[,] matrix = GridMatrix(grid, height, width);
+            Console.WriteLine("Matrix:");
             for (int vert = 0; vert < height; vert++)
             {
                 for (int hor = 0; hor < width; hor++)
-                {
-                    Console.Write(array[vert, hor] + " ");
-                }
+                    Console.Write(matrix[vert, hor] + " ");
                 Console.WriteLine();
+            }
+
+
+            string[] array = GridArray(grid, height, width);
+            Console.WriteLine("Array:");
+            for (int index = 1; index <= array.Length; index++)
+            {
+                if (index % width != 0)
+                    Console.Write(array[index - 1] + " ");
+                else
+                    Console.WriteLine(array[index - 1]);
             }
         }
 
@@ -26,12 +35,9 @@ namespace gridmatrix
         {
             string[,] result = new string[height, width];
 
-            // Map maidenhead grid onto continuous grid
+            // Map maidenhead grid onto a continuous grid
             int lonsq = 10 * (square.Substring(0, 1)[0] - 'A') + int.Parse(square.Substring(2, 1));
             int latsq = 10 * (square.Substring(1, 1)[0] - 'A') + int.Parse(square.Substring(3, 1));
-
-            //Console.WriteLine(string.Format("Longitude square #{0}", lonsq));
-            //Console.WriteLine(string.Format("Latitude square #{0}", latsq));
 
             for (int vert = 0; vert < height; vert++)
             {
@@ -59,20 +65,17 @@ namespace gridmatrix
         {
             string[] result = new string[height * width];
 
-            // Map maidenhead grid onto continuous grid
+            // Map maidenhead grid onto a continuous grid
             int lonsq = 10 * (square.Substring(0, 1)[0] - 'A') + int.Parse(square.Substring(2, 1));
             int latsq = 10 * (square.Substring(1, 1)[0] - 'A') + int.Parse(square.Substring(3, 1));
-
-            //Console.WriteLine(string.Format("Longitude square #{0}", lonsq));
-            //Console.WriteLine(string.Format("Latitude square #{0}", latsq));
 
             for (int vert = 0; vert < height; vert++)
             {
                 for (int hor = 0; hor < width; hor++)
                 {
-                    // Real square in continuous grid corresponding to vert and hor
-                    int thisvert = latsq - (vert - (height - 1) / 2);
-                    int thishor = lonsq + (hor - (width - 1) / 2);
+                    int maxp1 = 10 * ('R' - 'A' + 1);
+                    int thisvert = (maxp1 + latsq - (vert - (height - 1) / 2)) % maxp1;
+                    int thishor = (maxp1 + lonsq + (hor - (width - 1) / 2)) % maxp1;
 
                     // Form name of grid
                     string firstc = ((char)(thishor / 10 + 'A')).ToString();
