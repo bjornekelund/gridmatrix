a4e54bbca46b7b34e9d6e0e533c008dde7c4a2b6
Björn Ekelund <bjorn@ekelund.nu>
2021-04-01 14:02:25 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -6,18 +6,30 @@ namespace gridmatrix
     {
         static void Main(string[] args)
         {
-            string grid = "RR85MR";
+            string grid = "AA00MR";
             int height = 11;
             int width = 11;
 
-            string[,] matrix = GridMatrix(grid, height, width);
 
             // 0,0 is upper left corner, height-1,width-1 is lower right corner
-            for (int vert = 0; vert < height; vert++)
+            Console.WriteLine("Matrix:");
+            string[,] matrix = GridMatrix(grid, height, width);
+            for (int row = 0; row < height; row++)
             {
-                for (int hor = 0; hor < width; hor++)
-                    Console.Write(matrix[vert, hor] + " ");
-                Console.WriteLine();
+                for (int col = 0; col < width - 1; col++)
+                    Console.Write(matrix[row, col] + " ");
+                Console.WriteLine(matrix[row, width - 1]);
+            }
+
+            // 0 is upper left corner, height*width-1 is lower right corner
+            Console.WriteLine("Array:");
+            string[] array = GridArray(grid, height, width);
+            for (int index = 1; index <= height * width; index++)
+            {
+                if (index % width != 0)
+                    Console.Write(array[index - 1] + " ");
+                else
+                    Console.WriteLine(array[index - 1]);
             }
         }
 
@@ -43,10 +55,43 @@ namespace gridmatrix
                     int contHor = (wrap + lonsq + (col - (width - 1) / 2)) % wrap;
 
                     // Convert location back to grid square format
-                    result[row, col] = string.Format("{0}{1}{2}{3}", 
-                        (char)(contHor / 10 + 'A'), 
-                        (char)(contVer / 10 + 'A'), 
-                        contHor % 10, 
+                    result[row, col] = string.Format("{0}{1}{2}{3}",
+                        (char)(contHor / 10 + 'A'),
+                        (char)(contVer / 10 + 'A'),
+                        contHor % 10,
+                        contVer % 10);
+                }
+            }
+
+            return result;
+        }
+
+        static private string[] GridArray(string square, int height, int width)
+        {
+            string[] result = new string[height * width];
+
+            // Map center maidenhead grid onto a continuous 150 by 150 grid
+            // Maidenhead system has origin in south-west
+            // The created matrix has 0,0 in upper left corner
+            int lonsq = 10 * (square.Substring(0, 1)[0] - 'A') + int.Parse(square.Substring(2, 1));
+            int latsq = 10 * (square.Substring(1, 1)[0] - 'A') + int.Parse(square.Substring(3, 1));
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    // Calculate the current square's location in the continuous grid 
+                    // Make letter wrap around from R to A and vice versa
+                    // Make number wrap around from 9 to 0 and vice versa
+                    int wrap = 10 * ('S' - 'A');
+                    int contVer = (wrap + latsq - (row - (height - 1) / 2)) % wrap;
+                    int contHor = (wrap + lonsq + (col - (width - 1) / 2)) % wrap;
+
+                    // Convert location back to grid square format
+                    result[row + col * width] = string.Format("{0}{1}{2}{3}",
+                        (char)(contHor / 10 + 'A'),
+                        (char)(contVer / 10 + 'A'),
+                        contHor % 10,
                         contVer % 10);
                 }
             }
