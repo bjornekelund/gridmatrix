fcb7acae03811080761ac7d17fcc0185371cfdd8
Björn Ekelund <bjorn@ekelund.nu>
2021-04-01 09:44:41 +0200

    First

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gridmatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
